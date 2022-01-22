a96040c42bc5879168fbec1b2c6c2f698cbab5e9
Sridhar Rajaram <srilumia@hotmail.com>
2022-01-22 14:19:50 +0530

    Reverted back

--- src/App.js
+++ src/App.js
@@ -1,62 +1,32 @@
-import "./App.css";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import { useState } from "react";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+import axios from "axios";
+import { UrlLink } from "./UrlSettings";
 
-import { Link } from "react-router-dom";
-import { Container, Navbar, Nav } from "react-bootstrap";
-import RoutesBar from "./routes/RoutesBar";
 
 function App() {
-  const [token, setToken] = useState("");
-  const [email, setEmail] = useState("");
-  const [loginState, setLoginState] = useState("fail");
+  const [expense, setExpense] = useState([]);
 
+  useEffect(() => {
+    async function fetch() {
+      let data = await axios.get(`${UrlLink}/expense`);
+      setExpense(data.data);
+    }
+    fetch();
+  }, []);
+
+  const addExpenseHandler = (newExpense) => {
+    setExpense((prevExpenses) => {
+      return [newExpense, ...prevExpenses];
+    });
+  };
+  
   return (
-    <>
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Container>
-          {loginState === "fail" ? (
-            <Navbar.Brand href="/">Money Manager Application</Navbar.Brand>
-          ) : (
-            ""
-          )}
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="nav-links">
-              {loginState === "fail" ? (
-                <>
-                  <Link to="/SignUp" className="nav-link">
-                    Sign up
-                  </Link>
-                  <Link to="/Login" className="nav-link">
-                    Login
-                  </Link>
-                  <Link to="/Forgot" className="nav-link">
-                    Forgot password
-                  </Link>
-                </>
-              ) : (
-                <>
-                  <Link to="/expensetracker" className="nav-link">
-                    Expense Tracker
-                  </Link>
-                </>
-              )}{" "}
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-
-      <RoutesBar
-        loginState={loginState}
-        setLoginState={setLoginState}
-        token={token}
-        setToken={setToken}
-        email={email}
-        setEmail={setEmail}
-      />
-    </>
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expense} />
+    </div>
   );
 }
 
